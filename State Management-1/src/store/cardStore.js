aef216822e95bec41474fb37365abd9067edd595
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-11-25 23:02:08 +0530

    persis the data in the local storage by using zustand

--- State Management-1/src/store/cardStore.js
+++ State Management-1/src/store/cardStore.js
@@ -1,23 +1,30 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+export const useStore = create(
+  persist(
+    (set) => ({
+      cart: ["a", "b"],
+      num: 1,
 
-export const useStore = create((set) => ({
-  cart: ["a", "b"],
-  num: 1,
+      addtocart: (item) =>
+        set((state) => ({
+          cart: [...state.cart, item],
+        })),
 
-  addtocart: (item) =>
-    set((state) => ({
-      cart: [...state.cart, item],
-    })),
+      removeFromCart: (id) =>
+        set((state) => ({
+          cart: state.cart.filter((item) => item.id !== id),
+        })),
 
-  removeFromCart: (id) =>
-    set((state) => ({
-      cart: state.cart.filter((item) => item.id !== id),
-    })),
+      clearCart: () =>
+        set({
+          cart: [],
+        }),
 
-  clearCart: () =>
-    set({
-      cart: [],
+      addnum: () => set((state) => ({ num: state.num + 1 })),
     }),
-
-    addnum : () => set((state) => ({num : state.num + 1}))
-}));
+    {
+      name: "store-data",
+    }
+  )
+);
